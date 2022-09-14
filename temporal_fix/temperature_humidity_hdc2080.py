dc27d83f647e32cbef4da08b43bc52d7410dbe0a
Michael Smith <smithatlanta@gmail.com>
2022-09-14 14:47:14 -0400

    Update temperature_humidity_hdc2080.py

--- temporal_fix/temperature_humidity_hdc2080.py
+++ temporal_fix/temperature_humidity_hdc2080.py
@@ -5,7 +5,6 @@ import Pi_HDC2080
 hdc2000 = Pi_HDC2080.Pi_HDC2080()
 
 while True:
-    pressure = bmp280.get_pressure()
     print("temperature : %3.1f, humidity : %3.1f " %
           (hdc2000.readTemperature(), hdc2000.readHumidity()))
     time.sleep(1)
